fa9c362e874474d23206308a3fe996e2d302baaa
GiovaniMenegat <gi.menegat10@gmail.com>
2023-03-23 16:21:24 -0300

    feat: open bottom bar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,55 @@
-import { useEffect, useState } from "react"
+import { CaretDown, CaretUp } from "phosphor-react";
+import { useState } from "react";
 import { Clock } from "../../components/Clock";
 import { Quote } from "../../components/Quotes";
-import { HomeContainer } from "./styles";
+import { ButtonSvg, ExtraInformation, ExtraInformationButton, HomeBottom, HomeContainer } from "./styles";
 
 export function Home() {
+    const [openExtraInfo, setOpenExtraInfo] = useState(false)
 
+    function handleScrollToBottom() {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth",
+        });
+        setOpenExtraInfo(true)
+    }
+
+    function handleScrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+        setOpenExtraInfo(false)
+    }
+    
     return (
+        <>
         <HomeContainer>
             <Quote />
-            <Clock />
+            <HomeBottom>
+                <Clock />
+                {
+                    !openExtraInfo ?
+                    <ExtraInformationButton onClick={handleScrollToBottom}>
+                        MORE 
+                        <ButtonSvg>
+                            <CaretDown size={18} weight="bold" />
+                        </ButtonSvg>
+                    </ExtraInformationButton> :
+                    <ExtraInformationButton onClick={handleScrollToTop}>
+                        LESS 
+                        <ButtonSvg>
+                            <CaretUp size={18} weight="bold" />
+                        </ButtonSvg>
+                    </ExtraInformationButton>
+                }
+            </HomeBottom>
         </HomeContainer>
+
+        <ExtraInformation>
+            <h1>test teste stesd</h1>
+        </ExtraInformation>
+        </>
     )
 }
